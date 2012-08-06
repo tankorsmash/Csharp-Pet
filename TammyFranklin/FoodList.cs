84e080bb6bd41ae736d2f54148f3f0fd6cbddf28
Tankor Smash <tankorsmash@gmail.com>
2012-08-06 01:10:31 -0400

    added foodlist.cs

---
+++ TammyFranklin/FoodList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetR1
+{
+    
+    class  FoodType
+    {
+        public int nutrientsGiven = 15;
+        public  FoodType()
+        {
+            
+        }
+
+    }
+
+    class FoodMeat : FoodType
+    {
+        //access the nutrientsGiven here
+        public FoodMeat()
+        {
+            this.nutrientsGiven = 30;
+       }
+    }
+}
