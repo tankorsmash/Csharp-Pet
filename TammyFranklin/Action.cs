a379ab284bf0e327da999249fd44b28515fe4912
Tankor Smash <tankorsmash@gmail.com>
2012-08-10 22:50:46 -0400

    Refactored, created Action and Creature class files Pet is now a subclass of Creature and most of the generic methods of Pet is now Creature methods. This will be for enemy creatures. Moved Action class to it's own separate file. Not happy with the way it's working, but for now, it's all I've got

---
+++ TammyFranklin/Action.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetR1
+{
+    class Action
+        {
+            //The user who's action this is
+            User master;
+
+            public Action(User master)
+            {
+                this.master = master;
+            }
+
+        }
+
+    class BattleAction : Action
+    {
+        public BattleAction(User master)
+            : base(master)
+        {
+
+        }
+
+        public void BattlePet(Pet pet, Creature enemy)
+        {
+            Tools.Print("RAWR LET'S GO FIGHTING");
+        }
+
+    }
+
+    class FeedAction : Action
+    {
+        public FeedAction(User master)
+            : base(master)
+        {
+
+            //nothing happens
+        }
+
+        //Feeds the pet a Foodtype
+        public void FeedPet(Pet pet, FoodType foodToEat)
+        {
+            //feed the pet food
+            int gainedNuts = pet.food.Eat(foodToEat);
+
+            string text = "";
+
+            if (gainedNuts > 0)
+            {
+                text = String.Format("{0} just ate {1},",
+                                        pet.name,
+                                        foodToEat.ToString().ToLower());
+            }
+            //print the 
+            text = text + "{0} satiation level is: {1}\n";
+            string formatted = String.Format(text,
+                                            pet.pronoun,
+                                            pet.food.satiation);
+            Tools.Print(newFG: Program.defaultFG,
+                        text: formatted,
+                        newBG: ConsoleColor.DarkGreen);
+
+            //vals: new object[] {pet.name, foodToEat, pet.food.satiation});
+        }
+    }
+
+        //Printing out a screen of stats for the current pet
+    class StatAction : Action
+    {
+        public User master;
+        public StatAction(User master)
+            : base(master)
+        {
+            this.master = master;
+        }
+
+
+        public void CheckPetStats(Pet pet)
+        {
+            Tools.Print(newFG: ConsoleColor.Gray,
+            text: "Current Pet's name {0}\n" +
+                         "Current Satiation {1}\n" +
+                         "Current EXP {2}\n",
+              vals: new object[] {pet.name,
+                             pet.food.satiation,
+                             pet.exp});
+        }
+    }
+}
---
+++ TammyFranklin/Creature.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetR1
+{
+    class Creature
+    {
+
+        //pet details
+        public string name;
+        public int level = 0;
+        public long exp = 0;
+
+        public char gender = 'm';
+        public string pronoun
+        {
+            get
+            {
+                if (gender == 'm')
+                {
+                    return "his";
+                }
+                else if (gender == 'f')
+                {
+                    return "her";
+                }
+                else
+                {
+                    return "it's";
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("A {0} pet named {1}", this.getGender(), this.name);
+        }
+
+        /// <summary>
+        /// Returns the gender of the Pet, either full word "male" or a single letter "m"
+        /// </summary>
+        /// <param name="fullWord"></param>
+        /// <returns>"male" or "m"</returns>
+        public string getGender(bool fullWord = true)
+        {
+            if (fullWord)
+            {
+                if (this.gender == 'm')
+                {
+                    return "male";
+                }
+                else
+                {
+                    return "female";
+                }
+            }
+            else
+            {
+                return this.gender.ToString();
+            }
+        }
+    }
+
+    class Pet : Creature
+    {
+
+
+        public User master;
+
+        public PetMoodComponent mood;
+        public PetFoodComponent food;
+
+        public Pet(User newMaster, string newName, char newGender = 'm')
+        {
+            this.master = newMaster;
+            this.name = newName;
+            this.gender = newGender;
+
+            //assign components
+            this.mood = new PetMoodComponent(this);
+            this.food = new PetFoodComponent(this);
+
+            Console.WriteLine("A new {1} lion was created, named {0}, belonging to {2}",
+                this.name, this.getGender(), this.master.name);
+            //constructor method
+        }
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Adds exp to the pet's total, and changes the level if needed
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns>a long with the total exp</returns>
+        public long raiseExp(long newExp)
+        {
+            this.exp += newExp;
+            long printedExp;
+            printedExp = printExp();
+            return printedExp;
+        }
+
+        //Writes the current exp to the console and returns a long of the current exp
+        public long printExp()
+        {
+            Console.WriteLine("This is {0}'s total exp : {1}", this.name, this.exp);
+            return this.exp;
+        }
+
+
+
+        public void changeName(string newName)
+        {
+            this.name = newName;
+            Console.WriteLine("Pets new name: {0}", name);
+        }
+    }
+
+}
